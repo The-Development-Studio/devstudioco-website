626aa49eb21d5d110afc9196fd61c65e345362e6
Somaskandhan S <somaskandhan@devstudioco.com>
2025-10-24 23:58:03 +0530

    Add build.js wrapper to fix Vercel build

---
+++ build.js
@@ -0,0 +1,8 @@
+import { build } from 'vite';
+
+build().then(() => {
+  console.log('Vite build completed successfully.');
+}).catch((err) => {
+  console.error('Vite build failed:', err);
+  process.exit(1);
+});
